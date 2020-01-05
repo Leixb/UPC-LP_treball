049ea79e69a40d0d58987105d88dcc36ea02a35c
Leixb <abone9999@gmail.com>
2020-01-05 22:51:11 +0100

    Visitor guarda totes les dades al pickle

--- cl/EnquestesVisitor.py
+++ cl/EnquestesVisitor.py
@@ -1,101 +1,109 @@
-# Generated from Enquestes.g4 by ANTLR 4.7.2
+#!/usr/bin/env python3
 
 import networkx as nx
 
-from antlr4 import *
+from antlr4 import ParseTreeVisitor
+
 if __name__ is not None and "." in __name__:
     from .EnquestesParser import EnquestesParser
 else:
     from EnquestesParser import EnquestesParser
 
-# This class defines a complete generic visitor for a parse tree produced by EnquestesParser.
+
+class Node:
+    def __init__(self, identificador, tipus, data=None):
+        self.id = identificador
+        self.tipus = tipus
+        self.data = data
+
+    def __hash__(self):
+        return hash(self.id)
+
 
 class EnquestesVisitor(ParseTreeVisitor):
-
     def __init__(self):
         self.G = nx.DiGraph()
-        self.G.add_node("END")
         self.data = dict()
-        self.id_enquesta = ''
-        self.edge_labels = dict()
+        self.add_node(Node("END", "END"))
+        self.id_enquesta = ""
 
     def id_preg(self, item_id):
-        return self.data[item_id]['id_preg']
+        return self.data[item_id]["id_preg"]
+
+    def get_node(self, identificador):
+        return self.data[identificador]
+
+    def add_node(self, node):
+        self.G.add_node(node)
+        self.data[node.id] = node
+
+    def add_edge(self, a, b, **kwargs):
+        self.G.add_edge(self.get_node(a), self.get_node(b), **kwargs)
 
     # Visit a parse tree produced by EnquestesParser#root.
-    def visitRoot(self, ctx:EnquestesParser.RootContext):
-        n_children =  ctx.getChildCount()
+    def visitRoot(self, ctx: EnquestesParser.RootContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
-        l = [self.visit(i) for i in l[:-1]]
+        for i in l[:-1]:
+            self.visit(i)
 
-        items = self.data[self.id_enquesta]['items']
+        items = self.data[self.id_enquesta].data
         prev = items[0]
         for i in items[1:]:
-            self.G.add_edge(self.id_preg(prev), self.id_preg(i))
+            self.add_edge(self.id_preg(prev), self.id_preg(i))
             prev = i
-        self.G.add_edge(self.id_enquesta, self.id_preg(items[0]))
-        self.G.add_edge(self.id_preg(prev), "END")
+        self.add_edge(self.id_enquesta, self.id_preg(items[0]))
+        self.add_edge(self.id_preg(prev), "END")
 
-        return l
-
+        return self.G
 
     # Visit a parse tree produced by EnquestesParser#enquesta.
-    def visitEnquesta(self, ctx:EnquestesParser.EnquestaContext):
-        n_children =  ctx.getChildCount()
+    def visitEnquesta(self, ctx: EnquestesParser.EnquestaContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
         return [self.visit(i) for i in l[:-2]]
 
-
     # Visit a parse tree produced by EnquestesParser#preg.
-    def visitPreg(self, ctx:EnquestesParser.PregContext):
-        n_children =  ctx.getChildCount()
+    def visitPreg(self, ctx: EnquestesParser.PregContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
-        pregunta = dict()
-        pregunta["tipus"] = "pregunta"
-        pregunta["id"] = self.visit(l[0])
-        pregunta["text"] = self.visit(l[4])
+        ident = self.visit(l[0])
+        text = self.visit(l[4])
 
-        self.data[pregunta["id"]] = pregunta
-        self.G.add_node(pregunta["id"])
-        return pregunta
-
+        node = Node(ident, "pregunta", text)
+        self.add_node(node)
 
     # Visit a parse tree produced by EnquestesParser#text_pregunta.
-    def visitText_pregunta(self, ctx:EnquestesParser.Text_preguntaContext):
-        n_children =  ctx.getChildCount()
+    def visitText_pregunta(self, ctx: EnquestesParser.Text_preguntaContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [str(next(g)) for i in range(n_children)]
-        return ' '.join(l)
-
+        return " ".join(l)
 
     # Visit a parse tree produced by EnquestesParser#resp.
-    def visitResp(self, ctx:EnquestesParser.RespContext):
-        n_children =  ctx.getChildCount()
+    def visitResp(self, ctx: EnquestesParser.RespContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
-        resposta = dict()
-        resposta["tipus"] = "resposta"
-        resposta["id"] = self.visit(l[0])
-        resposta["opcions"] = self.visit(l[4])
-        self.data[resposta['id']] = resposta
-        self.G.add_node(resposta["id"])
-        return resposta
 
+        ident = self.visit(l[0])
+        opcions = self.visit(l[4])
+        node = Node(ident, "resposta", opcions)
+        self.add_node(node)
 
     # Visit a parse tree produced by EnquestesParser#opcions_resposta.
-    def visitOpcions_resposta(self, ctx:EnquestesParser.Opcions_respostaContext):
-        n_children =  ctx.getChildCount()
+    def visitOpcions_resposta(self, ctx: EnquestesParser.Opcions_respostaContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [self.visit(next(g)) for i in range(n_children)]
         return l
 
-
     # Visit a parse tree produced by EnquestesParser#opcio_resposta.
-    def visitOpcio_resposta(self, ctx:EnquestesParser.Opcio_respostaContext):
-        n_children =  ctx.getChildCount()
+    def visitOpcio_resposta(self, ctx: EnquestesParser.Opcio_respostaContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
         opcio = dict()
@@ -104,83 +112,67 @@ class EnquestesVisitor(ParseTreeVisitor):
         return opcio
         return self.visitChildren(ctx)
 
-
     # Visit a parse tree produced by EnquestesParser#text_opcio.
-    def visitText_opcio(self, ctx:EnquestesParser.Text_opcioContext):
-        n_children =  ctx.getChildCount()
+    def visitText_opcio(self, ctx: EnquestesParser.Text_opcioContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [str(next(g)) for i in range(n_children)]
-        text = ' '.join(l)
-        return text.split(';')[0].strip()
-
+        text = " ".join(l)
+        return text.split(";")[0].strip()
 
     # Visit a parse tree produced by EnquestesParser#id_opcio.
-    def visitId_opcio(self, ctx:EnquestesParser.Id_opcioContext):
+    def visitId_opcio(self, ctx: EnquestesParser.Id_opcioContext):
         g = ctx.getChildren()
         return int(next(g).getText())
         return self.visitChildren(ctx)
 
-
     # Visit a parse tree produced by EnquestesParser#item.
-    def visitItem(self, ctx:EnquestesParser.ItemContext):
-        n_children =  ctx.getChildCount()
+    def visitItem(self, ctx: EnquestesParser.ItemContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
-        item = dict()
-        item["tipus"] = "item"
-        item["id"] = self.visit(l[0])
-        item["id_preg"] = self.visit(l[4])
-        item["id_resp"] = self.visit(l[6])
-        edge = (item["id_preg"], item["id_resp"])
-        self.G.add_edge(*edge)
-        self.edge_labels[edge] = item["id"]
-        self.data[item['id']] = item
-        return item
-
+        ident = self.visit(l[0])
+        id_preg = self.visit(l[4])
+        id_resp = self.visit(l[6])
+        edge = (id_preg, id_resp)
+        self.add_edge(*edge, id=ident, tipus="item")
+        self.data[ident] = {"id_preg": id_preg, "id_resp": id_resp}
 
     # Visit a parse tree produced by EnquestesParser#id_preg.
-    def visitId_preg(self, ctx:EnquestesParser.Id_pregContext):
+    def visitId_preg(self, ctx: EnquestesParser.Id_pregContext):
         return ctx.getText()
-
 
     # Visit a parse tree produced by EnquestesParser#id_resp.
-    def visitId_resp(self, ctx:EnquestesParser.Id_respContext):
+    def visitId_resp(self, ctx: EnquestesParser.Id_respContext):
         return ctx.getText()
 
-
     # Visit a parse tree produced by EnquestesParser#alte.
-    def visitAlte(self, ctx:EnquestesParser.AlteContext):
-        n_children =  ctx.getChildCount()
+    def visitAlte(self, ctx: EnquestesParser.AlteContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
-        alternativa = dict()
-        alternativa["tipus"] = "alternativa"
-        alternativa["id"] = self.visit(l[0])
-        alternativa["id_item"] = self.visit(l[4])
-        alternativa["alternatives"] = self.visit(l[5])
-        self.data[alternativa['id']] = alternativa
 
-        id_pregunta = self.id_preg(alternativa["id_item"])
+        ident = self.visit(l[0])
+        id_item = self.visit(l[4])
+        alternatives = self.visit(l[5])
 
-        for i in alternativa["alternatives"]:
-            edge = (id_pregunta, self.id_preg(i['id_item']))
-            self.edge_labels[edge] = str(i['id_opcio'])
-            self.G.add_edge(*edge)
-        return alternativa
+        id_pregunta = self.id_preg(id_item)
 
+        for i in alternatives:
+            edge = (id_pregunta, self.id_preg(i["id_item"]))
+            self.add_edge(*edge, id_opcio=i["id_opcio"], tipus="alternativa")
 
     # Visit a parse tree produced by EnquestesParser#alternatives.
-    def visitAlternatives(self, ctx:EnquestesParser.AlternativesContext):
-        n_children =  ctx.getChildCount()
+    def visitAlternatives(self, ctx: EnquestesParser.AlternativesContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
         l = l[1:-1]
         return [self.visit(i) for i in l]
 
-
     # Visit a parse tree produced by EnquestesParser#alternativa.
-    def visitAlternativa(self, ctx:EnquestesParser.AlternativaContext):
-        n_children =  ctx.getChildCount()
+    def visitAlternativa(self, ctx: EnquestesParser.AlternativaContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
         alternativa = dict()
@@ -188,26 +180,22 @@ class EnquestesVisitor(ParseTreeVisitor):
         alternativa["id_item"] = self.visit(l[3])
         return alternativa
 
-
     # Visit a parse tree produced by EnquestesParser#enqu.
-    def visitEnqu(self, ctx:EnquestesParser.EnquContext):
-        n_children =  ctx.getChildCount()
+    def visitEnqu(self, ctx: EnquestesParser.EnquContext):
+        n_children = ctx.getChildCount()
         g = ctx.getChildren()
         l = [next(g) for i in range(n_children)]
-        enquesta = dict()
-        enquesta['id'] = self.visit(l[0])
-        enquesta['tipus'] = "enquesta"
-        enquesta['items'] = [self.visit(i) for i in l[4:-1]]
-        self.data[enquesta['id']] = enquesta
-        self.id_enquesta = enquesta['id']
-        self.G.add_node(enquesta['id'])
-        return enquesta
+        ident = self.visit(l[0])
+        self.id_enquesta = ident
 
+        items = [self.visit(i) for i in l[4:-1]]
+
+        node = Node(ident, "enquesta", items)
+        self.add_node(node)
 
     # Visit a parse tree produced by EnquestesParser#identificador.
-    def visitIdentificador(self, ctx:EnquestesParser.IdentificadorContext):
+    def visitIdentificador(self, ctx: EnquestesParser.IdentificadorContext):
         return ctx.getText()
-
 
 
 del EnquestesParser
--- cl/test.py
+++ cl/test.py
@@ -1,39 +1,88 @@
-import sys
+#!/usr/bin/env python3
 
-from antlr4 import *
-from EnquestesLexer import EnquestesLexer
-from EnquestesParser import EnquestesParser
-from EnquestesVisitor import EnquestesVisitor
-from antlr4.InputStream import InputStream
+import sys
+import pickle
 
 import matplotlib.pyplot as plt
 import networkx as nx
 
-if len(sys.argv) > 1:
-    input_stream = FileStream(sys.argv[1], encoding='utf-8')
+from antlr4 import FileStream, CommonTokenStream
+from antlr4.InputStream import InputStream
 
-else:
-    input_stream = InputStream(input('? '))
-lexer = EnquestesLexer(input_stream)
-token_stream = CommonTokenStream(lexer)
-parser = EnquestesParser(token_stream)
-tree = parser.root()
+from EnquestesLexer import EnquestesLexer
+from EnquestesParser import EnquestesParser
+from EnquestesVisitor import EnquestesVisitor
 
-visitor = EnquestesVisitor()
-print(visitor.visit(tree))
 
-#nx.draw_circular(visitor.G, with_labels=True)
+PICKLE_FILE = "network_graph.pckl"
+PLOT_FILE = "network_graph.png"
 
-G = visitor.G
 
-pos = nx.circular_layout(G)
-plt.figure()
-nx.draw(G, pos, edge_color='black', width=1,linewidths=1,\
-node_size=500, node_color='pink', alpha=0.9,\
-labels={node:node for node in G.nodes()})
-nx.draw_networkx_edge_labels(G,pos,edge_labels=visitor.edge_labels, font_color='red')
+def main():
 
-plt.axis('off')
-plt.savefig('foo.png')
+    if len(sys.argv) > 1:
+        input_stream = FileStream(sys.argv[1], encoding="utf-8")
+    else:
+        input_stream = InputStream(input("? "))
 
-#print(tree.toStringTree(recog=parser))
+    graph = parse_to_network(input_stream)
+
+    save_pickle(graph, PICKLE_FILE)
+    G = read_pickle(PICKLE_FILE)
+
+    plot_network(G, PLOT_FILE)
+
+
+def parse_to_network(input_stream) -> nx.DiGraph:
+    lexer = EnquestesLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = EnquestesParser(token_stream)
+    tree = parser.root()
+
+    visitor = EnquestesVisitor()
+    return visitor.visit(tree)
+
+
+def read_pickle(filename: str) -> nx.DiGraph:
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+def save_pickle(G: nx.DiGraph, filename: str):
+    with open(filename, "wb") as file:
+        pickle.dump(G, file)
+
+
+def plot_network(G: nx.DiGraph, filename: str):
+
+    pos = nx.circular_layout(G)
+    plt.figure()
+
+    nx.draw(
+        G,
+        pos,
+        edge_color="black",
+        width=1,
+        linewidths=1,
+        node_size=500,
+        node_color="pink",
+        alpha=0.9,
+        labels={node: node.id for node in G.nodes()},
+    )
+
+    edge_labels = nx.get_edge_attributes(G, "id")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color="red")
+
+    edge_labels_opcio = nx.get_edge_attributes(G, "id_opcio")
+    nx.draw_networkx_edge_labels(
+        G, pos, edge_labels=edge_labels_opcio, font_color="blue"
+    )
+
+    plt.axis("off")
+    plt.savefig(filename)
+
+
+if __name__ == "__main__":
+    main()
+
+# print(tree.toStringTree(recog=parser))
