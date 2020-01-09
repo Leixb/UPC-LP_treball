b9bf5e41fa59ff459617a3f3628c777f9af3cb45
Leixb <abone9999@gmail.com>
2020-01-09 13:06:30 +0100

    Allow multiple enquestes

--- cl/test.py
+++ cl/test.py
@@ -17,8 +17,10 @@ from EnquestesVisitor import EnquestesVisitor
 PICKLE_FILE = "network_graph.pckl"
 PLOT_FILE = "network_graph.png"
 
+plt.ioff()
 
 def main():
+    """Funcio principal, inicialitza l'entrada, guarda en un pickle i genera una gràfica."""
 
     if len(sys.argv) > 1:
         input_stream = FileStream(sys.argv[1], encoding="utf-8")
@@ -28,12 +30,13 @@ def main():
     graph = parse_to_network(input_stream)
 
     save_pickle(graph, PICKLE_FILE)
-    G = read_pickle(PICKLE_FILE)
+    graph_restored = read_pickle(PICKLE_FILE)
 
-    plot_network(G, PLOT_FILE)
+    plot_network(graph_restored, PLOT_FILE)
 
 
 def parse_to_network(input_stream) -> nx.DiGraph:
+    """Parseja i retorna el graph."""
     lexer = EnquestesLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = EnquestesParser(token_stream)
@@ -44,45 +47,62 @@ def parse_to_network(input_stream) -> nx.DiGraph:
 
 
 def read_pickle(filename: str) -> nx.DiGraph:
+    """Recupera el graph del pickle."""
     with open(filename, "rb") as file:
         return pickle.load(file)
 
 
-def save_pickle(G: nx.DiGraph, filename: str):
+def save_pickle(graph: nx.DiGraph, filename: str):
+    """Guarda el graph a un pickle."""
     with open(filename, "wb") as file:
-        pickle.dump(G, file)
+        pickle.dump(graph, file)
 
 
-def plot_network(G: nx.DiGraph, filename: str):
+def plot_network(graph: nx.DiGraph, filename: str):
+    """Genera i guarda la gràfica del arbre."""
 
-    pos = nx.circular_layout(G)
+    pos = nx.circular_layout(graph)
     plt.figure()
 
-    nx.draw(
-        G,
-        pos,
-        edge_color="black",
-        width=1,
-        linewidths=1,
-        node_size=500,
-        node_color="pink",
-        alpha=0.9,
-        labels={node: node for node in G.nodes()},
-    )
+    draw_nodes(graph, pos, "pregunta", "deepskyblue")
+    draw_nodes(graph, pos, "resposta", "khaki")
+    draw_nodes(graph, pos, "enquesta", "lightgreen")
+    draw_nodes(graph, pos, "END", "pink")
 
-    edge_labels = nx.get_edge_attributes(G, "id")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color="red")
+    node_labels = {node: node for node in graph.nodes()}
+    nx.draw_networkx_labels(graph, pos, labels=node_labels)
 
-    edge_labels_opcio = nx.get_edge_attributes(G, "id_opcio")
-    nx.draw_networkx_edge_labels(
-        G, pos, edge_labels=edge_labels_opcio, font_color="blue"
-    )
+    draw_edges(graph, pos, "id", "blue")
+    draw_edges(graph, pos, "id_opcio", "green")
+    draw_edges(graph, pos, "id_enq", "black")
 
     plt.axis("off")
     plt.savefig(filename)
 
 
+def draw_edges(graph: nx.DiGraph, pos, attr: str, color: str, **kwargs):
+    """Draws graph edges that have the given attribute."""
+    edge_labels = nx.get_edge_attributes(graph, attr)
+    edge_labels = {k: flatten(v) for k, v in edge_labels.items()}
+    nx.draw_networkx_edges(
+        graph, pos, edgelist=edge_labels.keys(), edge_color=color, **kwargs
+    )
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_color=color)
+
+
+def flatten(obj):
+    if not isinstance(obj, list):
+        return obj
+    return ",".join(obj)
+
+
+def draw_nodes(graph: nx.DiGraph, pos, tipus: str, color: str, **kwargs):
+    """Draws graph nodes that have the given "tipus"."""
+    node_list = [
+        n for n, d in graph.nodes(data=True) if "tipus" in d and d["tipus"] == tipus
+    ]
+    nx.draw_networkx_nodes(graph, pos, nodelist=node_list, node_color=color, **kwargs)
+
+
 if __name__ == "__main__":
     main()
-
-# print(tree.toStringTree(recog=parser))
